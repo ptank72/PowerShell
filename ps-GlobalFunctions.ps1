f95a3a832fb273eeefbe9f0317fdd75568c125c5
ptank72 <38856827+ptank72@users.noreply.github.com>
2019-04-16 08:13:59 -0400

    Update ps-GlobalFunctions.ps1

--- ps-GlobalFunctions.ps1
+++ ps-GlobalFunctions.ps1
@@ -1,9 +1,11 @@
 function showVar()
 {
+	[cmdletbinding()]
+	PARAM()
 	# clear screen and write global variable
 	cls
 	# using write-output, show global variable
-	Write-Output "'$myVar' is a global variable!"
+	Write-Verbose "'$myVar' is a global variable!"
 
 	return;
 }
@@ -11,4 +13,4 @@ function showVar()
 $global:myVar = "Hello World"
 
 # call function to show global variable
-showVar
+showVar -Verbose
